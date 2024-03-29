44e176e9a57afd1d2f124fc22cd7057302a772dc
Shynsa <61347233+Mirella972@users.noreply.github.com>
2024-03-29 00:16:32 -0400

    Ajout des entités acteur, realisateur et categorie

---
+++ CineQuebec.Windows/DAL/Data/Categorie.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineQuebec.Windows.DAL.Data
+{
+    public class Categorie
+    {
+        public ObjectId Id { get; set; }
+        public string Nom { get; set; }
+    }
+}
