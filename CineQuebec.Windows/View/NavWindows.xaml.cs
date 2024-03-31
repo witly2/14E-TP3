4af13b1586cbb037f28b543d5bbea19fdb1b424c
Shynsa <61347233+Mirella972@users.noreply.github.com>
2024-03-31 17:42:17 -0400

    Rebase main

--- CineQuebec.Windows/View/NavWindows.xaml.cs
+++ CineQuebec.Windows/View/NavWindows.xaml.cs
@@ -1,4 +1,7 @@
 ﻿
+using CineQuebec.Windows.BLL.Services;
+using CineQuebec.Windows.DAL.Repositories;
+using CineQuebec.Windows.DAL;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -10,10 +13,15 @@ namespace CineQuebec.Windows.View
     /// </summary>
     public partial class NavWindows : Window
     {
+        private readonly FilmService _filmService;
         public NavWindows()
         {
+            DatabaseGestion db = new DatabaseGestion();
+            _filmService = new FilmService(new FilmRepository(db));
+
             InitializeComponent();
             mainContentControl.Content = new AdminHomeControl();
+
         }
 
 
@@ -24,6 +32,11 @@ namespace CineQuebec.Windows.View
 
         }
 
-     
+        private void ToggleButton_Films_Click(object sender, RoutedEventArgs e)
+        {
+            mainContentControl.Content = new FilmsControl(_filmService);
+        }
+
+
     }
 }
