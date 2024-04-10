5b0c99cb38655163448f65a30b18a2629a730158
Shynsa <61347233+Mirella972@users.noreply.github.com>
2024-04-09 23:43:41 -0400

    GetFilms_Success

--- CineQuebec.WindowsTests/FilmServiceTests.cs
+++ CineQuebec.WindowsTests/FilmServiceTests.cs
@@ -23,8 +23,14 @@ namespace CineQuebec.WindowsTests
         {
             List<Film> expectedFilms = new List<Film>();
             Film film1 = new Film("Inception", "Inception", "Un voleur qui entre dans les rêves des autres pour voler leurs secrets de leur subconscient.", 148, new DateTime(2010, 7, 16), 8);
+            Film film2 = new Film("The Dark Knight", "Le Chevalier Noir", "Lorsque la menace connue sous le nom du Joker sème le chaos parmi les habitants de Gotham, Batman doit accepter l'une des plus grandes épreuves psychologiques et physiques de sa capacité à lutter contre l'injustice.", 152, new DateTime(2008, 7, 18), 9);
             expectedFilms.Add(film1);
+            expectedFilms.Add(film2);
+            _mockFilmRepository.Setup(x => x.GetFilms()).ReturnsAsync(expectedFilms);
 
+            var result = await _filmService.GetFilms();
+
+            Assert.AreEqual(expectedFilms, result);
         }
     }
 }
