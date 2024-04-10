101b3807d4f78a0777af054d3bede4d80850b580
Shynsa <61347233+Mirella972@users.noreply.github.com>
2024-04-09 22:50:09 -0400

    creation FilmServiceTests

--- CineQuebec.WindowsTests/FilmServiceTests.cs
+++ CineQuebec.WindowsTests/FilmServiceTests.cs
@@ -1,4 +1,7 @@
 using CineQuebec.Windows.BLL.Services;
+using CineQuebec.Windows.DAL.Data;
+using CineQuebec.Windows.DAL.Repositories.Films;
+using Moq;
 
 namespace CineQuebec.WindowsTests
 {
@@ -6,10 +9,18 @@ namespace CineQuebec.WindowsTests
     public class FilmServiceTests
     {
         private FilmService _filmService;
+        private Mock<IFilmRepository> _mockFilmRepository;
         [TestInitialize]
         public void Init()
         {
-            _filmService = new FilmService();
+            _mockFilmRepository = new Mock<IFilmRepository>();
+            _filmService = new FilmService(_mockFilmRepository.Object);
+        }
+
+        [TestMethod()]
+        public async Task GetFilms_Success()
+        {
+            
         }
     }
 }
