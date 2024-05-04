8e84138599e5b7df5adb8ee6a422b963a8dddfad
Wily Tatow <witly136@gmail.com>
2024-05-04 01:48:00 -0400

    Film viewModel

---
+++ CineQuebec.Windows/ViewModels/AccueilVM.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using CineQuebec.Windows.DAL;
+using CineQuebec.Windows.DAL.Data;
+using CineQuebec.Windows.DAL.Repositories.Films;
+using System.Linq;
+
+
+namespace CineQuebec.Windows.ViewModels;
+
+public class AccueilVM:Utilities.ViewModelBase
+{
+    private readonly FilmRepository _filmRepository;
+    private readonly DatabaseGestion _db;
+    private ObservableCollection<Film> _films;
+    
+    public ObservableCollection<Film>? Films
+    {
+        get { return _films; }
+        set
+        {
+            _films = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public AccueilVM()
+    {
+        _db = new DatabaseGestion();
+        _filmRepository = new FilmRepository(_db);
+        Films = new ObservableCollection<Film>(_filmRepository.GetFilms().Result);
+    }
+}
