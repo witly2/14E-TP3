ed37ea6e9309b262dec79877af6f72d62649b40a
Wily Tatow <witly136@gmail.com>
2024-05-04 00:18:22 -0400

    Création de role

--- CineQuebec.Windows/View/ConnexionControl.xaml.cs
+++ CineQuebec.Windows/View/ConnexionControl.xaml.cs
@@ -3,21 +3,12 @@ using CineQuebec.Windows.DAL;
 using CineQuebec.Windows.DAL.Data;
 using CineQuebec.Windows.DAL.Repositories.Abonnes;
 using CineQuebec.Windows.Utilities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace CineQuebec.Windows.View
 {
