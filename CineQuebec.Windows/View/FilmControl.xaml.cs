f21b22fc013dce7b373d5130a9c3bb5ab928c90a
Wily Tatow <witly136@gmail.com>
2024-05-04 20:03:03 -0400

    slider et Affichage des films

--- CineQuebec.Windows/View/FilmControl.xaml.cs
+++ CineQuebec.Windows/View/FilmControl.xaml.cs
@@ -1,14 +1,41 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 using CineQuebec.Windows.ViewModels;
 
 namespace CineQuebec.Windows.View;
 
 public partial class FilmControl : UserControl
 {
+    private int imgSlider=0;
+    private DispatcherTimer timer;
     public FilmControl()
     {
         InitializeComponent();
         AccueilVM filmVM = new AccueilVM();
         this.DataContext = filmVM;
+        
+        timer = new DispatcherTimer();
+        timer.Interval = TimeSpan.FromSeconds(4); 
+        timer.Tick += Timer_Tick; 
+        timer.Start(); 
+    }
+    
+    private void LoadNextImage()
+    {
+        if (imgSlider==3)
+            imgSlider=0;
+        string imagePath = $"../../../Ressources/images/{imgSlider}.png";
+
+        BitmapImage bitmapImage = new BitmapImage(new Uri(imagePath, UriKind.RelativeOrAbsolute));
+
+        ImgBrushSlider.ImageSource = bitmapImage;
+
+        imgSlider++;
+    }
+    
+    private void Timer_Tick(object sender, EventArgs e)
+    {
+        LoadNextImage();
     }
 }
