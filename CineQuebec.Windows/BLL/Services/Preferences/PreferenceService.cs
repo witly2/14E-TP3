17f05d7dccf291829c03d12748a04d76ba5d9e3e
Shynsa <61347233+Mirella972@users.noreply.github.com>
2024-05-07 12:53:29 -0400

    Creation de la class Preference et du test AddPreferenceRealisateur

---
+++ CineQuebec.Windows/BLL/Services/Preferences/PreferenceService.cs
@@ -0,0 +1,25 @@
+﻿using CineQuebec.Windows.DAL.Data;
+using CineQuebec.Windows.DAL.Repositories.Preferences;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineQuebec.Windows.BLL.Services.Preferences
+{
+    public class PreferenceService: IPreferenceService
+    {
+        private readonly IPreferenceRepository _preferenceRepository;
+
+        public PreferenceService(IPreferenceRepository preferenceRepository)
+        {
+            _preferenceRepository = preferenceRepository;
+        }
+
+        public void AddPreferenceRealisateur(Preference preference, Realisateur realisateur)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
