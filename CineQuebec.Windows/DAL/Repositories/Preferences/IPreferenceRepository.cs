90f94e446ff645e42bb038b602cf0d9f69c5cc5a
Shynsa <61347233+Mirella972@users.noreply.github.com>
2024-05-08 00:30:36 -0400

    Ajout des tests pour ajout plus de 5 realisateurs preferes et si realisateur est deja dans la liste des preferences.

--- CineQuebec.Windows/DAL/Repositories/Preferences/IPreferenceRepository.cs
+++ CineQuebec.Windows/DAL/Repositories/Preferences/IPreferenceRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CineQuebec.Windows.DAL.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace CineQuebec.Windows.DAL.Repositories.Preferences
 {
     public interface IPreferenceRepository
     {
+        bool IsRealisateurAlreadyInList(Preference preference, Realisateur realisateur);
+        void UpdatePreference(Preference preference);
     }
 }
--- CineQuebec.WindowsTests/PreferenceTests/PreferenceServiceTests.cs
+++ CineQuebec.WindowsTests/PreferenceTests/PreferenceServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using CineQuebec.Windows.BLL.Services.Preferences;
 using CineQuebec.Windows.DAL.Data;
+using CineQuebec.Windows.DAL.Exceptions;
 using CineQuebec.Windows.DAL.Repositories.Preferences;
 using Moq;
 using System.ComponentModel.Design;
@@ -12,7 +13,7 @@ namespace CineQuebec.WindowsTests.PreferenceTests
         private readonly IPreferenceRepository _mockPreferenceRepository = new Mock<IPreferenceRepository>().Object;
 
         [TestMethod]
-        public void AddPreferenceRealisateur_ShouldAddRealisateurPreferenceToList()
+        public void AddPreferenceRealisateurLessFive_ShouldAddRealisateurPreferenceToList()
         {
             var preferenceService = new PreferenceService(_mockPreferenceRepository);
             var preference = new Preference();
@@ -21,6 +22,37 @@ namespace CineQuebec.WindowsTests.PreferenceTests
             preferenceService.AddPreferenceRealisateur(preference, realisateur);
 
             Assert.IsTrue(preference.ListPreferenceRealisateur.Contains(realisateur));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MaximumPreferenceException))]
+        public void AddPreferenceRealisateurMoreThanFive_ShouldThrowException()
+        {
+            var preferenceService = new PreferenceService(_mockPreferenceRepository);
+            var preference = new Preference();
+            var list = new List<Realisateur>()
+            {
+                new Realisateur("Nolan", "Christopher"),
+                new Realisateur("Chazelle", "Damien"),
+                new Realisateur("Villeneuse", "Denis"),
+                new Realisateur("Tarantino", "Quentin"),
+                new Realisateur("Ford Coppola", "Francis")
+            };
+            preference.SetListPreferenceRealisateur(list);
+
+            preferenceService.AddPreferenceRealisateur(preference, new Realisateur("Spielberg", "Steven"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(PreferenceAlreadyExistsException))]
+        public void AddExistingPreferenceRealisateur_ShouldThrowException()
+        {
+            var preferenceService = new PreferenceService(_mockPreferenceRepository);
+            var preference = new Preference();
+            var realisateur = new Realisateur("Tarentino", "Quentin");
+
+            preferenceService.AddPreferenceRealisateur(preference, realisateur);
+            preferenceService.AddPreferenceRealisateur(preference, realisateur);
         }
     }
 }
