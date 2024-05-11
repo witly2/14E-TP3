2ec3a6782c42527a496c81bd4dbda7ee3321504c
Shynsa <61347233+Mirella972@users.noreply.github.com>
2024-05-11 13:44:14 -0400

    tests recompensesService

---
+++ CineQuebec.Windows/BLL/Services/Recompenses/IRecompenseService.cs
@@ -0,0 +1,14 @@
+﻿using CineQuebec.Windows.DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineQuebec.Windows.BLL.Services.Recompenses
+{
+    public interface IRecompenseService
+    {
+        Task<Recompense> AjouterRecompenseAvantPremiere(Recompense recompenseExpected);
+    }
+}
