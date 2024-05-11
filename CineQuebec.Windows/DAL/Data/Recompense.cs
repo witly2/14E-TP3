2c156f4b578b329f07300952ab4f253f5ac4ac04
Shynsa <61347233+Mirella972@users.noreply.github.com>
2024-05-11 11:43:55 -0400

    Creation class Recompense

---
+++ CineQuebec.Windows/DAL/Data/Recompense.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineQuebec.Windows.DAL.Data
+{
+    public class Recompense
+    {
+        [BsonId]
+        public ObjectId Id { get; private set; }
+        public Abonne Abonne { get; private set; }
+        public TypeRecompense Type { get; private set; }
+        public Projection? Projection { get; private set; }
+        public DateTime? DateExpiration { get; private set; }
+    }
+}
---
+++ CineQuebec.Windows/DAL/Data/TypeRecompense.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineQuebec.Windows.DAL.Data
+{
+    public enum TypeRecompense
+    {
+        InvitationAvantPremiere,
+        TicketGratuit
+    }
+}
