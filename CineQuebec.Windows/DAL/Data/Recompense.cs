22340071a393008333815bd4a95f49bb9e80b8df
Shynsa <61347233+Mirella972@users.noreply.github.com>
2024-05-11 13:44:14 -0400

    tests recompensesService

--- CineQuebec.Windows/DAL/Data/Recompense.cs
+++ CineQuebec.Windows/DAL/Data/Recompense.cs
@@ -16,5 +16,38 @@ namespace CineQuebec.Windows.DAL.Data
         public TypeRecompense Type { get; private set; }
         public Projection? Projection { get; private set; }
         public DateTime? DateExpiration { get; private set; }
+
+        public Recompense(Abonne abonne, TypeRecompense type, Projection projection)
+        {
+            Abonne = abonne;
+            Type = type;
+            Projection = projection;
+        }
+
+        public Recompense(Abonne abonne, TypeRecompense type, DateTime dateExpiration)
+        {
+            Abonne = abonne;
+            Type = type;
+            DateExpiration = dateExpiration;
+        }
+
+        public void SetAbonne(Abonne abonne)
+        {
+            Abonne = abonne;
+        }
+
+        public void SetTypeRecompense(TypeRecompense type)
+        {
+            Type = type;
+        }
+
+        public void SetProjection(Projection projection)
+        {
+            Projection = projection;
+        }
+        public void SetDateExpiration(DateTime dateExpiration)
+        {
+            DateExpiration = dateExpiration;
+        }
     }
 }
---
+++ CineQuebec.Windows/DAL/Repositories/Recompenses/IRecompenseRepository.cs
@@ -0,0 +1,14 @@
+﻿using CineQuebec.Windows.DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineQuebec.Windows.DAL.Repositories.Recompenses
+{
+    public interface IRecompenseRepository
+    {
+        Task<Recompense> AjouterRecompenseAvantPremiere(Recompense recompense);
+    }
+}
