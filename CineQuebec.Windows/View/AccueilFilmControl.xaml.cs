0b8333123cd65a93a9f10859e2c15863882ebf5d
Wily Tatow <witly136@gmail.com>
2024-05-12 07:44:37 -0400

    image et ficheier

---
+++ CineQuebec.Windows/View/AccueilFilmControl.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using CineQuebec.Windows.ViewModels;
+
+namespace CineQuebec.Windows.View;
+
+public partial class AccueilFilmControl : UserControl
+{
+    public AccueilFilmControl()
+    {
+        InitializeComponent();
+        var filmViewModel = new AccueilFilmViewModel();
+        DataContext = filmViewModel;
+
+        timer = new DispatcherTimer();
+        timer.Interval = TimeSpan.FromSeconds(4);
+        timer.Tick += Timer_Tick;
+        timer.Start();
+    }
+    
+    private int imgSlider;
+    private readonly DispatcherTimer timer;
+
+
+
+    private void LoadNextImage()
+    {
+        if (imgSlider == 3)
+            imgSlider = 0;
+        var imagePath = $"../../../Ressources/images/{imgSlider}.png";
+
+        var bitmapImage = new BitmapImage(new Uri(imagePath, UriKind.RelativeOrAbsolute));
+
+        ImgBrushSlider.ImageSource = bitmapImage;
+
+        imgSlider++;
+    }
+
+    private void Timer_Tick(object sender, EventArgs e)
+    {
+        LoadNextImage();
+    }
+}
