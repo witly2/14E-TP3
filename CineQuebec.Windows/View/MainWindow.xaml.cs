a269327b0c1855c1c27b59dc2a18cd7d4adb73b0
Shynsa <61347233+Mirella972@users.noreply.github.com>
2024-05-12 12:36:59 -0400

    Creation structure recompense UI

--- CineQuebec.Windows/View/MainWindow.xaml.cs
+++ CineQuebec.Windows/View/MainWindow.xaml.cs
@@ -2,15 +2,40 @@
 using System.Windows.Input;
 using CineQuebec.Windows.BLL.Services;
 using CineQuebec.Windows.BLL.Services.Connexion;
+using CineQuebec.Windows.BLL.Services.Recompenses;
 using CineQuebec.Windows.DAL;
 using CineQuebec.Windows.DAL.Data;
 using CineQuebec.Windows.DAL.Repositories.Abonnes;
 using CineQuebec.Windows.DAL.Repositories.Films;
 using CineQuebec.Windows.DAL.Repositories.Persons;
 using CineQuebec.Windows.DAL.Repositories.Projections;
+using CineQuebec.Windows.DAL.Repositories.Recompenses;
 using CineQuebec.Windows.View;
-using CineQuebec.Windows.ViewModels;
+using CineQuebec.Windows.View.RecompenseAdminView;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
+namespace CineQuebec.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly FilmService _filmService;
+        private readonly AbonneService _abonneService;
+        private readonly ProjectionService _projectionService;
+        private readonly RecompenseService _recompenseService;
+        private readonly ConnexionService _connexionService;
+        private readonly Film film;
 namespace CineQuebec.Windows;
 
 /// <summary>
@@ -25,13 +50,14 @@ public partial class MainWindow : Window
     private readonly ConnexionService _connexionService;
     private AccueilFilmControl accueilFilmControl;
 
-    public MainWindow()
-    {
-        var db = new DatabaseGestion();
-        _filmService = new FilmService(new FilmRepository(db));
-        _abonneService = new AbonneService(new AbonneRepsitory(db));
-        _projectionService = new ProjectionService(new ProjectionRepository(db));
-        _connexionService = new ConnexionService(new PersonRepository(db));
+        public MainWindow()
+        {
+            DatabaseGestion db = new DatabaseGestion();
+            _filmService = new FilmService(new FilmRepository(db));
+            _abonneService = new AbonneService(new AbonneRepsitory(db));
+            _projectionService = new ProjectionService(new ProjectionRepository(db));
+            _recompenseService = new RecompenseService(new RecompenseRepository(db));
+            _connexionService = new ConnexionService(new PersonRepository(db));
 
 
         InitializeComponent();
@@ -74,6 +100,11 @@ public partial class MainWindow : Window
     {
         mainContentControl.Content = new AddProjectionControl(_projectionService, film);
     }
+
+        public void RecompenseView()
+        {
+            mainContentControl.Content = new RecompenseView(_recompenseService);
+        }
 
 
     private void click_MouseLeftButtonDown_1(object sender, MouseButtonEventArgs e)
