937092b1c5fb115d36ab757a50a5fb2bc9ddffd7
Wily Tatow <witly136@gmail.com>
2024-05-14 01:25:39 -0400

    ficchier

---
+++ CineQuebec.Windows/View/UsersControls/ReservationAbonneControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using CineQuebec.Windows.DAL.Data;
+using CineQuebec.Windows.ViewModels.AbonneViewModel;
+
+namespace CineQuebec.Windows.View.UsersControls;
+
+public partial class ReservationAbonneControl : UserControl
+{
+    public ReservationAbonneControl(Abonne abonne)
+    {
+        InitializeComponent();
+        var ReservationAbonneViewModel = new AbonneReservationViewModel(abonne);
+        DataContext = ReservationAbonneViewModel.Reservations;
+    }
+
+
+
+    private void Btn_Update_Reservation(object sender, RoutedEventArgs e)
+    {
+        var button = (Button)sender;
+        var reservation = (Reservation)button.DataContext;
+        var updateReservation = new UpdateReservationForm(reservation);
+        updateReservation.ShowDialog();
+    }
+}
